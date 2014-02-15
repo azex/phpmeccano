c98505edbb361f9dc32f434934b78383cac85195
Alexei Muzarov <azexmail@gmail.com>
2014-02-15 17:02:27 +0400

    Initial commit. It contains only framework configuration file.

---
+++ conf.php
@@ -0,0 +1,13 @@
+<?php
+// system configurations
+
+// database parameters
+define('MECCANO_DBANAME', 'root');
+define('MECCANO_DBAPASS', 'MySQLpassw');
+define('MECCANO_DBHOST', 'localhost');
+define('MECCANO_DBPORT', '3306');
+define('MECCANO_DBNAME', 'phpmeccano');
+define('MECCANO_TPREF', 'meccano');
+
+// system folders
+define('MECCANO_ROOT_DIR', dirname(__FILE__));
