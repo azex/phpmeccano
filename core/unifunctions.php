12d1e2d6825d36fba351091baf864c0f5f3618d4
Alexei Muzarov <azexmail@gmail.com>
2014-02-15 21:06:38 +0400

    There was added passwHash function for calculating of password hash

--- core/unifunctions.php
+++ core/unifunctions.php
@@ -11,3 +11,13 @@ function makeSalt($prefix = '') {
 function makeIdent($prefix = '') {
     return sha1(uniqid().microtime(TRUE).rand(1, 10e9).uniqid($prefix, TRUE));
 }
+
+// it calculates password hash
+function passwHash($password, $salt) {
+    if (is_string($password) && strlen($password)<51 && strlen($password)>7 && is_string($salt) && strlen($salt)==22) {
+        return sha1(crypt($password, '$2y$10$'.$salt));
+    }
+    else {
+        return FALSE;
+    }
+}
