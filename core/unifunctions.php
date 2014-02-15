deaf6e208c97bfeeb7a6c4c59e748a62803912ba
Alexei Muzarov <azexmail@gmail.com>
2014-02-15 19:54:13 +0400

    There was added unifunctions.php core module with first function for generating of unique password salt.

---
+++ core/unifunctions.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace core;
+
+// it generates and returns unique password salt
+function makeSalt($prefix = '') {
+    return substr(base64_encode(sha1(uniqid().microtime(TRUE).rand(1, 10e9).uniqid($prefix, TRUE))), 0, 22);
+}
