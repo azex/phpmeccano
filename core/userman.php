a113baf3f08f7a259c1827ed26882473756a4280
Alexei Muzarov <azexmail@gmail.com>
2014-05-27 20:44:32 +0500

    Was added method groupStatus.

--- core/userman.php
+++ core/userman.php
@@ -58,4 +58,44 @@ class UserMan {
         }
         return $groupId;
     }
+    
+    public static function groupStatus($id, $active, $log = TRUE) {
+        if (!is_integer($id) || $id<1) {
+            self::setErrId(ERROR_INCORRECT_DATA);            self::setErrExp('groupStatus: group id must be integer and greater than zero');
+            return FALSE;
+        }
+        if ($active) {
+            $active = 1;
+        }
+        elseif (!$active && $id>1) {
+            $active = 0;
+        }
+        else {
+            self::setErrId(ERROR_SYSTEM_INTERVENTION);            self::setErrExp('groupStatus: system group can\'t be disabled');
+            return FALSE;
+        }
+        self::$dblink->query("UPDATE `".MECCANO_TPREF."_core_userman_groups` "
+                . "SET `active`=$active "
+                . "WHERE `id`=$id ;");
+        if (self::$dblink->errno) {
+            self::setErrId(ERROR_NOT_EXECUTED);            self::setErrExp('groupStatus: status wasn\'t changed | '.self::$dblink->error);
+            return FALSE;
+        }
+        if (!self::$dblink->affected_rows) {
+            self::setErrId(ERROR_INCORRECT_DATA);            self::setErrExp('groupStatus: incorrect group status or group doesn\'t exist');
+            return FALSE;
+        }
+        if ($log) {
+            if ($active) {
+                $l = Logging::newRecord('core_enGroup', "$id");
+            }
+            else {
+                $l = Logging::newRecord('core_disGroup', "$id");
+            }
+            if (!$l) {
+                self::setErrId(ERROR_NOT_CRITICAL);            self::setErrExp(Logging::errExp());
+            }
+        }
+        return TRUE;
+    }
 }
