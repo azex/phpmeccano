1766ad8664e122e7d72729c5c6870c17ae3fed4d
Alexei Muzarov <azexmail@gmail.com>
2014-05-27 15:29:37 +0500

    Was added method createGroup.

--- core/swconst.php
+++ core/swconst.php
@@ -8,3 +8,4 @@ define('ERROR_INCORRECT_DATA', -1); //incorrect value or type of the data
 define('ERROR_NOT_EXECUTED', -2); //function or query was not executed
 define('ERROR_RESTRICTED_ACCESS', -3); //access to function is restricted by policy
 define('ERROR_SYSTEM_INTERVENTION', -4); //intervention that violates system structure
+define('ERROR_NOT_CRITICAL', -5); //not critical error
---
+++ core/userman.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace core;
+
+require_once 'swconst.php';
+require_once 'unifunctions.php';
+require_once 'logging.php';
+require_once 'policy.php';
+
+class UserMan {
+    private static $errid = 0; // error's id
+    private static $errexp = ''; // error's explanation
+    private static $dblink; // database link
+    
+    public function __construct($dblink = FALSE) {
+        self::$dblink = $dblink;
+    }
+    
+    public static function setDbLink($dblink) {
+        self::$dblink = $dblink;
+    }
+    
+    private static function setErrId($id) {
+        self::$errid = $id;
+    }
+    
+    private static function setErrExp($exp) {
+        self::$errexp = $exp;
+    }
+    
+    public static function errId() {
+        return self::$errid;
+    }
+    
+    public static function errExp() {
+        return self::$errexp;
+    }
+    
+    public static function createGroup($groupname, $description, $log = TRUE) {
+        if (!pregGName($groupname) || !is_string($description)) {
+            self::setErrId(ERROR_NOT_EXECUTED);            self::setErrExp('createGroup: incorect type of incoming parameters');
+            return FALSE;
+        }
+        $description = self::$dblink->real_escape_string($description);
+        self::$dblink->query("INSERT INTO `".MECCANO_TPREF."_core_userman_groups` (`groupname`, `description`) "
+                . "VALUES ('$groupname', '$description') ;");
+        if (self::$dblink->errno) {
+            self::setErrId(ERROR_NOT_EXECUTED);            self::setErrExp('createGroup: group wasn\'t created | '.self::$dblink->error);
+            return FALSE;
+        }
+        $groupId = self::$dblink->insert_id;
+        if (!Policy::addGroup($groupId)) {
+            self::setErrId(ERROR_NOT_EXECUTED);            self::setErrExp(Policy::errExp());
+            return FALSE;
+        }
+        if ($log && !Logging::newRecord('core_newGroup', $groupname)) {
+            self::setErrId(ERROR_NOT_CRITICAL);            self::setErrExp(Logging::errExp());
+        }
+        return $groupId;
+    }
+}
