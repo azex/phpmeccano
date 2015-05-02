9d3b72caa22fb21d84f18b1de58fe00bc5eaa4cb
Alexei Muzarov <azexmail@gmail.com>
2015-05-02 17:14:30 +0500

    Session variables [AUTH_USERNAME], [AUTH_LANGUAGE] and [AUTH_LANGUAGE_DIR] are updated at every call of the method [isSession].

--- core/auth.php
+++ core/auth.php
@@ -142,7 +142,7 @@ class Auth implements intAuth {
                 $this->setError(ERROR_NOT_EXECUTED, 'isSession: session probably is stolen');
                 return FALSE;
             }
-            $qResult = $this->dbLink->query("SELECT `g`.`groupname`, `u`.`id`, `p`.`password` "
+            $qResult = $this->dbLink->query("SELECT `l`.`code`, `l`.`dir`, `u`.`username`, `g`.`groupname`, `u`.`id`, `p`.`password` "
                     . "FROM `".MECCANO_TPREF."_core_userman_groups` `g` "
                     . "JOIN `".MECCANO_TPREF."_core_userman_users` `u` "
                     . "ON `g`.`id`=`u`.`groupid` "
@@ -150,6 +150,8 @@ class Auth implements intAuth {
                     . "ON `p`.`userid`=`u`.`id` "
                     . "JOIN `".MECCANO_TPREF."_core_auth_usi` `s` "
                     . "ON `s`.`id`=`p`.`id` "
+                    . "JOIN `".MECCANO_TPREF."_core_langman_languages` `l`"
+                    . "ON `l`.`id`=`u`.`langid` "
                     . "WHERE `u`.`id`=".$_SESSION[AUTH_USER_ID]." "
                     . "AND `u`.`active`=1 "
                     . "AND `g`.`active`=1 "
@@ -163,6 +165,10 @@ class Auth implements intAuth {
                 $this->userLogout();
                 return FALSE;
             }
+            $userData = $qResult->fetch_row();
+            $_SESSION[AUTH_LANGUAGE] = $userData[0];
+            $_SESSION[AUTH_LANGUAGE_DIR] = $userData[1];
+            $_SESSION[AUTH_USERNAME] = $userData[2];
             return TRUE;
         }
         return FALSE;
