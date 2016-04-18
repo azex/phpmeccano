fbd38a3c39b4cb91a5c094890e58065e1fa4db97
Alexei Muzarov <azexmail@gmail.com>
2016-04-18 15:51:39 +0400

    New method [editMsgComment].

--- core/share.php
+++ core/share.php
@@ -71,6 +71,7 @@ interface intShare {
     public function appendSubStripe($userId, $mtmark, $rpp = 20);
     public function updateSubStripe($userId, $mtmark);
     public function createMsgComment($msgId, $userId, $comment, $parentId = '');
+    public function editMsgComment($comment, $commentId, $userId);
 }
 
 class Share extends Discuss implements intShare {
@@ -3385,6 +3386,46 @@ class Share extends Discuss implements intShare {
         }
         else {
             $this->setError(ERROR_RESTRICTED_ACCESS, 'createMsgComment: access denied');
+            return FALSE;
+        }
+    }
+    
+    public function editMsgComment($comment, $commentId, $userId) {
+        $this->zeroizeError();
+        if(!pregGuid($commentId)) {
+            $this->setError(ERROR_INCORRECT_DATA, 'editMsgComment: incorrect parameters');
+            return FALSE;
+        }
+        $qTopic = $this->dbLink->query(
+                "SELECT `r`.`id` "
+                . "FROM `".MECCANO_TPREF."_core_share_msg_topic_rel` `r` "
+                . "JOIN `".MECCANO_TPREF."_core_discuss_comments` `c` "
+                . "ON `c`.`tid`=`r`.`tid` "
+                . "AND `c`.`id`='$commentId' ;"
+                );
+        if ($this->dbLink->errno) {
+            $this->setError(ERROR_NOT_EXECUTED, 'editMsgComment: unable to get message identifies -> '.$this->dbLink->error);
+            return FALSE;
+        }
+        if (!$this->dbLink->affected_rows) {
+            $this->setError(ERROR_NOT_FOUND, 'editMsgComment: message not found');
+            return FALSE;
+        }
+        list($msgId) = $qTopic->fetch_row();
+        if (isset($_SESSION[AUTH_USER_ID]) && $this->checkMsgAccess($msgId)) {
+            if ($this->editComment($comment, $commentId, $userId)) {
+                return TRUE;
+            }
+            else {
+                FALSE;
+            }
+        }
+        elseif ($this->errid) {
+            $this->setError($this->errid, 'editMsgComment -> '.$this->errexp);
+            return FALSE;
+        }
+        else {
+            $this->setError(ERROR_RESTRICTED_ACCESS, 'editMsgComment: access denied');
             return FALSE;
         }
     }
