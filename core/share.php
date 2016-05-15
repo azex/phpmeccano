23642d8e3efcefc67d2d015d5ecb8ef724b2fd14
Alexei Muzarov <azexmail@gmail.com>
2016-05-15 16:40:08 +0400

    Redundant output data have been removed from method [updateMsgStripe].

--- core/share.php
+++ core/share.php
@@ -2332,7 +2332,6 @@ class Share extends Discuss implements intShare {
                 $msgNode->appendChild($xml->createElement('title', htmlspecialchars($title)));
                 $msgNode->appendChild($xml->createElement('text', $text));
                 $msgNode->appendChild($xml->createElement('time', $msgTime));
-                $msgNode->appendChild($xml->createElement('mtmark', $mtMark));
                 $msgsNode->appendChild($msgNode);
             }
             else {
@@ -2341,8 +2340,7 @@ class Share extends Discuss implements intShare {
                     'source' => $source,
                     'title' => htmlspecialchars($title),
                     'text' => $text,
-                    'time' => $msgTime,
-                    'mtmark' => $mtMark
+                    'time' => $msgTime
                 );
             }
         }
