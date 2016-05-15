6d0fc29360d38824ee0558c5443fd35af57e4ed5
Alexei Muzarov <azexmail@gmail.com>
2016-05-15 19:30:54 +0400

    Redundant output data have been removed from method [subStripe].

--- core/share.php
+++ core/share.php
@@ -3234,7 +3234,6 @@ class Share extends Discuss implements intShare {
                 $msgNode->appendChild($xml->createElement('title', htmlspecialchars($title)));
                 $msgNode->appendChild($xml->createElement('text', $text));
                 $msgNode->appendChild($xml->createElement('time', $msgTime));
-                $msgNode->appendChild($xml->createElement('mtmark', $mtMark));
                 $msgsNode->appendChild($msgNode);
             }
             else {
@@ -3246,8 +3245,7 @@ class Share extends Discuss implements intShare {
                     'source' => $source,
                     'title' => htmlspecialchars($title),
                     'text' => $text,
-                    'time' => $msgTime,
-                    'mtmark' => $mtMark
+                    'time' => $msgTime
                 );
             }
         }
