a495900d1adf87eb1c7f8db1e1193c6af1bd4658
Alexei Muzarov <azexmail@gmail.com>
2018-10-21 15:03:23 +0400

    Added comments to some database tables.

--- install/webinstaller.php
+++ install/webinstaller.php
@@ -291,7 +291,7 @@ class WebInstaller extends ServiceMethods implements intWebInstaller {
                 `time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                 PRIMARY KEY (`id`),
                 UNIQUE KEY `name` (`name`)
-            ) ENGINE=$sEngine DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;",
+            ) ENGINE=$sEngine DEFAULT CHARSET=utf8 COMMENT 'Installed plug-ins' AUTO_INCREMENT=1 ;",
             
             // about installed plugin
             "CREATE TABLE `{$tabPrefix}_core_plugins_installed_about` (
@@ -304,7 +304,7 @@ class WebInstaller extends ServiceMethods implements intWebInstaller {
                 `license` text NOT NULL DEFAULT '' COMMENT 'License agreement',
                 FOREIGN KEY (`id`) REFERENCES `{$tabPrefix}_core_plugins_installed` (`id`),
                 PRIMARY KEY (`id`)
-            ) ENGINE=$sEngine DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;",
+            ) ENGINE=$sEngine DEFAULT CHARSET=utf8 COMMENT 'Information about installed plug-ins' AUTO_INCREMENT=1 ;",
             
             // about unpacked plugins
             "CREATE TABLE `{$tabPrefix}_core_plugins_unpacked` (
@@ -322,7 +322,7 @@ class WebInstaller extends ServiceMethods implements intWebInstaller {
                 `depends` text NOT NULL DEFAULT '' COMMENT 'Dependences needed for the plugin',
                 PRIMARY KEY (`id`),
                 UNIQUE KEY `short` (`short`)
-            ) ENGINE=$sEngine DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;",
+            ) ENGINE=$sEngine DEFAULT CHARSET=utf8 COMMENT 'Information about unpacked but not installed plug-ins' AUTO_INCREMENT=1 ;",
             
             // system languages
             "CREATE TABLE `{$tabPrefix}_core_langman_languages` (
