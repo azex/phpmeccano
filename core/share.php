9b354251b12b6f14e0533490b2fe61dd6cca7be0
Alexei Muzarov <azexmail@gmail.com>
2019-01-14 10:53:15 +0400

    Method [getMsgShares] was updated to support array output type.

--- core/share.php
+++ core/share.php
@@ -1413,8 +1413,11 @@ class Share extends Discuss implements intShare {
         if ($this->outputType == 'xml') {
             return $xml;
         }
-        else {
+        elseif ($this->outputType == 'json') {
             return json_encode($sharesNode);
+        }
+        else {
+            return $sharesNode;
         }
     }
     
