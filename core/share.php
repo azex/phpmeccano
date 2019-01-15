f2e3efcfbdd0fb261342d863e430f725aa72e0ab
Alexei Muzarov <azexmail@gmail.com>
2019-01-15 11:28:22 +0400

    Method [appendMsgStripe] was updated to support array output type.

--- core/share.php
+++ core/share.php
@@ -2310,7 +2310,12 @@ class Share extends Discuss implements intShare {
         }
         else {
             $msgsNode['minmark'] = (double) $minMark;
-            return json_encode($msgsNode);
+            if ($this->outputType == 'json') {
+                return json_encode($msgsNode);
+            }
+            else {
+                return $msgsNode;
+            }
         }
     }
     
