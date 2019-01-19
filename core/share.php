20aaf88c31576e4a535e8d4d18a0f2e83599067b
Alexei Muzarov <azexmail@gmail.com>
2019-01-20 03:44:13 +0400

    Method [appendFileStripe] was updated to support array output type.

--- core/share.php
+++ core/share.php
@@ -2964,7 +2964,12 @@ class Share extends Discuss implements intShare {
         }
         else {
             $filesNode['minmark'] = (double) $minMark;
-            return json_encode($filesNode);
+            if ($this->outputType == 'json') {
+                return json_encode($filesNode);
+            }
+            else {
+                return $filesNode;
+            }
         }
     }
     
