851ba1cf6e663ea4fe5eb2d2f42ce09154e6b675
Alexei Muzarov <azexmail@gmail.com>
2019-01-30 20:53:56 +0400

    Method [updateSubStripe] was updated to support array output type.

--- core/share.php
+++ core/share.php
@@ -3594,7 +3594,12 @@ class Share extends Discuss implements intShare {
         }
         else {
             $msgsNode['maxmark'] = (double) $maxMark;
-            return json_encode($msgsNode);
+            if ($this->outputType == 'json') {
+                return json_encode($msgsNode);
+            }
+            else {
+                return $msgsNode;
+            }
         }
     }
     
