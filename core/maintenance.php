f0a2994250c569be645de8c3aa1f2df6a8db9241
Alexei Muzarov <azexmail@gmail.com>
2019-03-10 16:15:15 +0400

    Methods [readConf] and [state] have bees united into the method [state]. Code has been updated to fit new structure of the configurational file. Method [writeConfig] has been renamed to [write].

--- core/maintenance.php
+++ core/maintenance.php
@@ -28,8 +28,7 @@ namespace core;
 loadPHP('extclass');
 
 interface intMaintenance {
-    public function readConfig();
-    public function writeConfig($conf);
+    public function write($conf);
     public function state();
     public function enable();
     public function disable();
@@ -37,7 +36,7 @@ interface intMaintenance {
 
 class Maintenance extends ServiceMethods implements intMaintenance {
     
-    public function readConfig() {
+    public function state() {
         $confPath = MECCANO_SERVICE_PAGES.'/maintenance.json';
         if (!is_file($confPath)) {
             $this->setError(ERROR_NOT_FOUND, "readConf: configurational file [$confPath] is not found");
@@ -58,6 +57,10 @@ class Maintenance extends ServiceMethods implements intMaintenance {
             $this->setError(ERROR_INCORRECT_DATA, 'readConf: parameter [timeout] is incorrect or not exist');
             return false;
         }
+        if (!isset($decoded->startpoint) || gettype($decoded->startpoint) != 'integer' || $decoded->startpoint<0) {
+            $this->setError(ERROR_INCORRECT_DATA, 'readConf: parameter [startpoint] is incorrect or not exist');
+            return false;
+        }
         if (!isset($decoded->prmsg) || gettype($decoded->prmsg) != 'string') {
             $this->setError(ERROR_INCORRECT_DATA, 'readConf: parameter [prmsg] is incorrect or not exist');
             return false;
@@ -66,77 +69,87 @@ class Maintenance extends ServiceMethods implements intMaintenance {
             $this->setError(ERROR_INCORRECT_DATA, 'readConf: parameter [secmsg] is incorrect or not exist');
             return false;
         }
-        return $conf;
+        if ($decoded->enabled && $decoded->timeout && ($decoded->timeout + $decoded->startpoint)< time()) {
+            $expired = true;
+        }
+        else {
+            $expired = false;
+        }
+        return array('enabled' => $decoded->enabled, 'prmsg' => $decoded->prmsg, 'secmsg' => $decoded->secmsg, 'timeout' => $decoded->timeout, 'startpoint' => $decoded->startpoint, 'expired' => $expired);
     }
     
-    public function writeConfig($conf) {
+    public function write($conf, $startpoint = 0) {
         if (gettype($conf) != 'object') {
-            $this->setError(ERROR_INCORRECT_DATA, 'writeConfig: invalid type of got parameters');
+            $this->setError(ERROR_INCORRECT_DATA, 'write: invalid type of got parameters');
             return false;
         }
         if (!isset($conf->enabled) || gettype($conf->enabled) != 'boolean') {
-            $this->setError(ERROR_INCORRECT_DATA, 'writeConfig: parameter [enabled] is incorrect or not exist');
-            return false;
-        }
-        if (!isset($conf->timeout) || gettype($conf->timeout) != 'integer' || $decoded->timeout<0) {
-            $this->setError(ERROR_INCORRECT_DATA, 'writeConfig: parameter [timeout] is incorrect or not exist');
+            $this->setError(ERROR_INCORRECT_DATA, 'write: parameter [enabled] is incorrect or not exist');
             return false;
         }
         if (!isset($conf->prmsg) || gettype($conf->prmsg) != 'string') {
-            $this->setError(ERROR_INCORRECT_DATA, 'writeConfig: parameter [prmsg] is incorrect or not exist');
+            $this->setError(ERROR_INCORRECT_DATA, 'write: parameter [prmsg] is incorrect or not exist');
             return false;
         }
         if (!isset($conf->secmsg) || gettype($conf->secmsg) != 'string') {
-            $this->setError(ERROR_INCORRECT_DATA, 'writeConfig: parameter [secmsg] is incorrect or not exist');
+            $this->setError(ERROR_INCORRECT_DATA, 'write: parameter [secmsg] is incorrect or not exist');
+            return false;
+        }
+        if (!isset($conf->timeout) || gettype($conf->timeout) != 'integer' || $conf->timeout<0) {
+            $this->setError(ERROR_INCORRECT_DATA, 'write: parameter [timeout] is incorrect or not exist');
+            return false;
+        }
+        if (gettype($startpoint) != 'integer' || $startpoint<0) {
+            $this->setError(ERROR_INCORRECT_DATA, 'write: parameter [$startpoint] is incorrect or not exist');
             return false;
         }
         $confPath = MECCANO_SERVICE_PAGES.'/maintenance.json';
         if (!is_file($confPath)) {
-            $this->setError(ERROR_NOT_FOUND, "writeConfig: configurational file [$confPath] is not found");
+            $this->setError(ERROR_NOT_FOUND, "write: configurational file [$confPath] is not found");
             return false;
         }
         if (!is_writable($confPath)) {
-            $this->setError(ERROR_RESTRICTED_ACCESS, "writeConfig: configurational file [$confPath] is not writable");
+            $this->setError(ERROR_RESTRICTED_ACCESS, "write: configurational file [$confPath] is not writable");
             return false;
         }
-        file_put_contents($confPath, json_encode($conf));
+        file_put_contents(
+                $confPath, json_encode(
+                        array(
+                            'enabled' => $conf->enabled,
+                            'prmsg' => $conf->prmsg,
+                            'secmsg' => $conf->secmsg,
+                            'timeout' => $conf->timeout,
+                            'startpoint' => $startpoint
+                            )
+                        )
+                );
         return true;
     }
     
-    public function state() {
-        $conf = $this->readConfig();
-        if (!$conf) {
-            $this->setError($this->errid, 'state -> '.$this->errexp);
-            return false;
-        }
-        $decoded = json_decode($conf);
-        return array('enabled' => $decoded->enabled);
-    }
-    
     public function enable() {
-        $conf = $this->readConfig();
+        $conf = $this->state();
         if (!$conf) {
             $this->setError($this->errid, 'enable -> '.$this->errexp);
             return false;
         }
-        $decoded = json_decode($conf);
+        $decoded = (object) $conf;
         if (!$decoded->enabled) {
             $decoded->enabled = true;
-            $this->writeConfig($decoded);
+            $this->write($decoded, time());
         }
         return array('enabled' => $decoded->enabled);
     }
     
     public function disable() {
-        $conf = $this->readConfig();
+        $conf = $this->state();
         if (!$conf) {
             $this->setError($this->errid, 'disable -> '.$this->errexp);
             return false;
         }
-        $decoded = json_decode($conf);
+        $decoded = (object) $conf;
         if ($decoded->enabled) {
             $decoded->enabled = false;
-            $this->writeConfig($decoded);
+            $this->write($decoded);
         }
         return array('enabled' => $decoded->enabled);
     }
